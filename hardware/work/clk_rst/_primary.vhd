156e53303ba77a88311fa0ffd6be7087af0b21a1
John Eslinger <johneslingertx@gmail.com>
2022-12-29 22:46:04 -0600

    Attempting to stop verilog/modelsim metadata from being saved by the repo

--- hardware/work/clk_rst/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity clk_rst is
-    port(
-        pwrOn           : in     vl_logic;
-        clk             : out    vl_logic;
-        rstn            : out    vl_logic
-    );
-end clk_rst;
