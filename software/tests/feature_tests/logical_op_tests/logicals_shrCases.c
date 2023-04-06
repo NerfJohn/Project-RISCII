a797b6784478be835935e233672247c062ceffd1
NerfJohn <johneslingertx@gmail.com>
2023-04-05 21:31:06 -0500

    Forgot char literal check.

--- software/tests/feature_tests/logical_op_tests/logicals_shrCases.c
+++ software/tests/feature_tests/logical_op_tests/logicals_shrCases.c
@@ -17,11 +17,13 @@ int main() {
   int shr4;
   int shr5;
   int shr6;
+  int shr7;
   
   // Decide by literal.
   shr0 = 0xffff >> 1;
   shr1 = -1 >> 1;
   shr2 = 1 >> 1;
+  shr7 = '\n' >> 1;
   
   // Decide by var.
   shr3 = int_t >> 1;
@@ -29,6 +31,6 @@ int main() {
   shr5 = char_t >> 1;
   shr6 = uchar_t >> 1;
   
-  // Return sum to check value (32767 + -1 + 0 + -1 + 32767 + -1 + 127 = 122 + 65536)
-  return shr0 + shr1 + shr2 + shr3 + shr4 + shr5 + shr6 + -121; 
+  // Return sum to check value (32767 + -1 + 0 + 5 + -1 + 32767 + -1 + 127 = 127 + 65536)
+  return shr0 + shr1 + shr2 + shr3 + shr4 + shr5 + shr6 + -126; 
 }
