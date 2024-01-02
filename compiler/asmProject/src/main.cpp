d471ee929e8edad129bc2340b1bb1a90284b4a4a
John Eslinger <johneslingertx@gmail.com>
2024-01-02 17:38:02 -0600

    Added folders/projects for processor and assembler development- also started 3rd party manifest for records

---
+++ compiler/asmProject/src/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Jan 1, 2024
+ *      Author: John
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "RISCII Assembler" << endl;
+}
+
---
+++ processor/alteraProject/src/AlteraProject.sv
@@ -0,0 +1,8 @@
+module AlteraProject(A, B, C);
+
+output A;
+input B, C;
+
+assign A = B | C;
+
+endmodule
