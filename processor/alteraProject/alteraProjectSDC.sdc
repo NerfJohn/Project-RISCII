b9cb8f69dbc319de4f383497bd3b32364c3e0d3d
John Eslinger <johneslingertx@gmail.com>
2024-01-03 23:10:33 -0600

    Added modules to ease interfacing to altera hardware for future work

---
+++ processor/alteraProject/alteraProjectSDC.sdc
@@ -0,0 +1,2 @@
+# Clock source
+create_clock -name clock_50 -period 20 [get_ports {CLOCK_50}]
--- processor/alteraProject/src/AlteraProject.sv
+++ processor/alteraProject/src/AlteraProject.sv
@@ -1,8 +1,26 @@
-module AlteraProject(A, B, C);
+module AlteraProject(SW, HEX, KEY, LEDR, LEDG,
+                     CLOCK_50
+);
 
-output A;
-input B, C;
+// Special inputs.
+input CLOCK_50; // 50 MHz clock signal
 
-assign A = B | C;
+// User inputs.
+input[3:0] SW;  // Switches- held high or low
+input[3:0] KEY; // Active low pushbuttons
+
+// User outputs.
+output[55:0] HEX; // 7-segment displays- [HEX7[6]:HEX0[0]] ordering
+output[3:0] LEDR; // Red LEDs
+output[3:0] LEDG; // Green LEDs
+
+// Helper modules for ease of resources.
+logic clkSignal;
+LazyPll #(.DIVIDER(1000000)) iClk (.inClk(CLOCK_50), .outClk(clkSignal));
+SegDisplay iDisplay[7:0] (.inNibble(32'b0), .outSegs(HEX));
+
+assign LEDR = SW;
+assign LEDG = {4{clkSignal}} & KEY;
+
 
 endmodule
