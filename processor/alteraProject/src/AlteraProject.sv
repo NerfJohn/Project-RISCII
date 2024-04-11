cf0f147474b05f93f17b35d3bc62d78e90cc770a
John Eslinger <johneslingertx@gmail.com>
2024-04-10 22:43:53 -0500

    Added connections to dev-kit SRAM chip and tested it at 8 MHz

--- processor/alteraProject/src/AlteraProject.sv
+++ processor/alteraProject/src/AlteraProject.sv
@@ -1,107 +1,116 @@
-module AlteraProject(SW, HEX, KEY, LEDR, LEDG, GPIO0,
-                     CLOCK_50
+////////////////////////////////////////////////////////////////////////////////
+// AlteraProject.sv: "Root/Chasis to connect boge circuits to physical FPGA"
+// ~John Eslinger
+////////////////////////////////////////////////////////////////////////////////
+
+module AlteraProject(
+	// Human/Direct Inputs.
+	input[7:0]   SW,       // On/Off switches (4 on either side of row)
+	input[3:0]   KEY,      // Push-buttons (active-low) 
+	
+	// Human/Direct Outputs.
+	output[55:0] HEX,      // 7-segment displays (mainly to show hex values)
+	output[3:0]  LEDR,     // Red LEDs
+	output[3:0]  LEDG,     // Green LEDs
+	
+	// Human/Direct Bi-directionals.
+	inout[7:0]   GPIO0,    // Basic GPIOs (left->right/up->down on board)
+	
+	// FPGA/board provided 50 MHz clock.
+	input        CLOCK_50,  // Clock signal
+	
+	// FPGA/board provided SRAM chip (512 KB, word-addressable).
+	output[17:0] SRAM_ADDR, // address
+	inout[15:0]  SRAM_DQ,   // data- for read and write
+	output       SRAM_WE_N, // write enable (active low)
+	output       SRAM_OE_N, // output enable (active low)
+	output       SRAM_UB_N, // upper byte mask
+	output       SRAM_LB_N, // lower byte mask
+	output       SRAM_CE_N
 );
 
-// Special inputs.
-input CLOCK_50; // 50 MHz clock signal
+///////////////////////////////
+// -- Chasis wires/blocks -- //
+///////////////////////////////
 
-// User inputs.
-input[7:0] SW;  // Switches (left/righmost 4)- held high or low
-input[3:0] KEY; // Active low pushbuttons
+// Wires to connect to PLL/Hex Display blocks.
+wire[31:0] _TO_HEX;
+wire       _FROM_CLK;
 
-// User outputs.
-output[55:0] HEX; // 7-segment displays- [HEX7[6]:HEX0[0]] ordering
-output[3:0] LEDR; // Red LEDs
-output[3:0] LEDG; // Green LEDs
+// Signal to treat as common "reset" signal.
+wire       _FROM_RSTN;
 
-// User Inouts.
-inout[7:0] GPIO0;
+// "PLL" block to divide provided clock to desired speed.
+LazyPll #(.DIVIDER(6)) iClk (
+	.inClk(CLOCK_50), 
+	.outClk(_FROM_CLK)
+);
 
-//==============================================================================
+// Converter from nibble to 7-segment controls (for hexadecimal value).
+SegDisplay iDisplay[7:0] (
+	.inNibble(_TO_HEX),
+	.outSegs(HEX)
+);
 
-// Intermediate signals (related to help modules).
-logic CLK_IN;
-logic[31:0] HEX_OUT;
+// Have KEY[0]/LEDG[0] (ie rightmost button/LED) control resetting.
+assign _FROM_RSTN = KEY[0];
+assign LEDG[0] = ~KEY[0];   // Light indicates active low reset
 
-// Intermediate signals.
-logic inTCK, inTMS, inTDI, outTDO;
-logic       inRSTN;
-logic[2:0]  sigSTATE;
-logic[7:0]  sigINSTR;
-logic[15:0] sigDATA;
-logic[15:0] sigADDR;
-logic[1:0]  sigSIGS;
+// Show clk speed on LEDG[1] (helps for slower speeds).
+assign LEDG[1] = _FROM_CLK;
 
-wire mem_isWr, mem_memEn;
-wire[15:0] mem_addr, mem_data;
+///////////////////////////////////////////////////////////////////
+// -- Semi-DUT: Experimental JTAG Controller aids development -- //
+///////////////////////////////////////////////////////////////////
 
-logic[5:0] sigTRIS;
-wire dut_pins, sdaLine, sigBYTE;
+// Test signals related to JTAG DUT.
+logic[15:0] test_jtagData;
 
-// Adjusted clock signal and Hex output modules.
-LazyPll #(.DIVIDER(50)) iClk (.inClk(CLOCK_50), .outClk(CLK_IN));
-SegDisplay iDisplay[7:0] (.inNibble(HEX_OUT), .outSegs(HEX));
+// Wires connecting JTAG controller to other DUTs/blocks.
+wire[15:0] jtagAddr, jtagData;
+wire jtagWr, jtagEn;
+wire jtagTck, jtagTdi, jtagTdo, jtagTms;
 
-// JTAG controller (for better debugging).
+// JTAG DUT instance.
 JtagMemController CTRL(
-	.addr(mem_addr),
-	.data(mem_data),
-	.isWr(mem_isWr),
-	.memEn(mem_memEn),
+	.addr(jtagAddr),
+	.data(SRAM_DQ),
+	.isWr(jtagWr),
+	.memEn(jtagEn),
 	
-	.tck(inTCK),
-	.tms(inTMS),
-	.tdi(inTDI),
-	.tdo(outTDO),
+	.tck(jtagTck),
+	.tms(jtagTms),
+	.tdi(jtagTdi),
+	.tdo(jtagTdo),
 	
-	.clk(CLK_IN),
-	.rstn(inRSTN),
+	.clk(_FROM_CLK),
+	.rstn(_FROM_RSTN),
 	
-	.test_state(sigSTATE),
-	.test_instr(sigINSTR),
-	.test_data(sigDATA),
-	.test_addr(sigADDR),
-	.test_sigs(sigSIGS)
+	.test_data(test_jtagData)
 );
 
-// DUT.
-I2cController DUT (
-	// Memory Bus Connections.
-	.I2cController_busAddr(mem_addr[1:0]),
-	.I2cController_busWr(mem_isWr),
-	.I2cController_busEn(mem_memEn),
-	.I2cController_busData(mem_data),
-	
-	// I2C pins/signals.
-	.I2cController_SCL(dut_pins),
-	.I2cController_SDA(sdaLine),
-	
-	.test_state(sigTRIS),
-	.test_data(sigBYTE),
-	
-	// Common Nets/Signals.
-	.clk(CLK_IN),
-	.rstn(inRSTN)
-);
+// Get JTAG signals from GPIOs.
+assign jtagTck = GPIO0[0];
+assign jtagTdi = GPIO0[1];
+assign jtagTms = GPIO0[2];
+assign GPIO0[3] = jtagTdo;
 
-// TODO-remove.
+// Use LEDG[3] to show when JTAG connection is active.
+assign LEDG[3] = jtagTck;
 
-// Human Input.
-assign inRSTN = KEY[0];
+////////////////////////////////////////////////
+// -- DUT: Module under active development -- //
+////////////////////////////////////////////////
 
-// "Link" IO.
-assign inTCK = GPIO0[0];	// "top left" Altera pin, pin 2 on nano
-assign inTDI = GPIO0[1];	// "top right" Altera pin, pin 3 on nano
-assign inTMS = GPIO0[2];	// "low left" Altera pin, pin 4 on nano
-assign GPIO0[3] = outTDO;	// "low right" Altera pin, pin 5 on nano
+// Attach JTAG DUT to SRAM chip on FPGA/board.
+assign SRAM_ADDR = {2'b00, jtagAddr};
+assign SRAM_WE_N = ~jtagWr;
+assign SRAM_OE_N = jtagWr;
+assign SRAM_UB_N = jtagWr & ~_FROM_CLK; // related to write trigger- sync w/ clk posedge
+assign SRAM_LB_N = jtagWr & ~_FROM_CLK; // related to write trigger- sync w/ clk posedge
+assign SRAM_CE_N = ~jtagEn;
 
-assign LEDR = GPIO0[3:0];
-
-assign GPIO0[5:4] = 2'b10;
-assign GPIO0[7:6] = {dut_pins, sdaLine};
-
-// Human Feedback.
-assign HEX_OUT = {7'h0, sigTRIS[5], 8'h0,3'h0, sigTRIS[4:0], 3'h0, sigBYTE, 3'h0, dut_pins};
-assign LEDG = ~KEY;
+// Watch JTAGs data.
+assign _TO_HEX = {jtagData, test_jtagData};
 
 endmodule
