d91cb9de2624648ffd6b3878f4b8231d8b44d385
John Eslinger <johneslingertx@gmail.com>
2024-06-07 16:33:28 -0500

    Saving off- memory is on/off working. Starting heavy re-work to try to make it more consistent

--- processor/DevProject/src/CoreBlocks/Alu.v
+++ processor/DevProject/src/CoreBlocks/Alu.v
@@ -28,8 +28,9 @@ module Alu (
 wire [15:0] andGateS, orGateS, xorGateS;
 
 // Wires/Nets related to barrel shifting computations.
-wire [1:0]  shiftOp1, shiftOp2, shiftOp3, shiftOp4;
-wire [15:0] shiftL1,  shiftL2,  shiftL3,  shiftS;
+wire [15:0] shiftLhs,    shiftS;
+wire [3:0]  shiftRhs;
+wire [1:0]  shiftOpcode;
 
 // Wires related to the adder computations.
 wire [15:0] adderA, adderB, adderS;
@@ -39,9 +40,26 @@ wire [15:0] adderI, adderO;
 wire [7:0]  setUpperByte;
 wire [15:0] setS;
 
+// Nets used to determine result of ALU.
+wire [15:0] opcode01X, opcode11X;
+
 ///////////////////////////////////////
 // -- Functional Blocks/Instances -- //
 ///////////////////////////////////////
+
+//------------------------------------------------------------------------------
+// 16-bit barrel shifter- used for shift operations (and sub-op flags).
+Shifter16 SHIFTER (
+	// Operand inputs.
+	.i_lhs(shiftLhs),
+	.i_rhs(shiftRhs),
+	
+	// Control input.
+	.i_opcode(shiftOpcode),
+	
+	// Result of shifting.
+	.o_result(shiftS)
+);
 
 //------------------------------------------------------------------------------
 // 16-bit adder- used for integer addition and subtraction computations.
@@ -64,11 +82,48 @@ assign orGateS  = i_src1 | i_src2;
 assign xorGateS = i_src1 ^ i_src2;
 
 //------------------------------------------------------------------------------
+// Compute "shift" operations.
+assign shiftLhs    = i_src1;
+assign shiftRhs    = i_src2[3:0];
+assign shiftOpcode = {i_opcode[0], ~i_flagBit};
 
-// TODO- actually implement; for now, just add to get LDR, STR, and SWP working.
-assign adderA = i_src1;
+//------------------------------------------------------------------------------
+// Compute "arithmetic" operations.
+assign adderA = i_src1 ^ {16{i_opcode[0]}};  // For SUB: invert A with NOT...
 assign adderB = i_src2;
-assign adderI = {adderO[14:0], 1'b0};
-assign o_result = adderS;
+assign adderI = {adderO[14:0], i_opcode[0]}; // ...and add 1
+
+//------------------------------------------------------------------------------
+// Compute "set" operations.
+Mux2 M0[7:0] (
+	.A(i_src1[7:0]),
+	.B({8{i_src2[7]}}),
+	.S(i_flagBit),
+	.Y(setUpperByte)
+);
+assign setS = {setUpperByte, i_src2[7:0]};
+
+//------------------------------------------------------------------------------
+// Select the result requested via opcode.
+Mux2 M1[15:0] (
+	.A(setS),
+	.B(xorGateS),
+	.S(i_opcode[0]),
+	.Y(opcode01X)
+);
+Mux2 M2[15:0] (
+	.A(orGateS),
+	.B(andGateS),
+	.S(i_opcode[0]),
+	.Y(opcode11X)
+);
+Mux4 M3[15:0] (
+	.A(adderS),
+	.B(opcode01X),
+	.C(shiftS),
+	.D(opcode11X),
+	.S(i_opcode[2:1]),
+	.Y(o_result)
+);
 
 endmodule
--- processor/DevProject/src/CoreBlocks/Shifter16.v
+++ processor/DevProject/src/CoreBlocks/Shifter16.v
@@ -16,23 +16,85 @@ module Shifter16 (
 );
 
 /*
- * TODO- desc. module (0-3 = no shift
+ * Shifter16 - implements 3/4 types of shifting for 16-bit numbers.
+ *
+ * Implements shift operations for RISCII's ALU. Shift operations are inferred
+ * as an extra opcode (apart from ALU's opcode). See below for available
+ * operations.
+ *
+ * == Shift Operations ==
+ * opcode | operation
+ * -------+----------
+ * 2'b00  | no shift (used to avoid shifting by certain digits)
+ * 2'b01  | left logical shift
+ * 2'b10  | right arithmetic shift
+ * 2'b11  | right logical shift
  */
 
 //////////////////////////////////
 // -- Internal Signals/Wires -- //
 //////////////////////////////////
 
-///////////////////////////////////////
-// -- Functional Blocks/Instances -- //
-///////////////////////////////////////
+// Wires related to computing each layer's select signals.
+wire [1:0]  selL1, selL2, selL3, selL4;
 
-//------------------------------------------------------------------------------
+// Wires related to the result of each shift layer.
+wire [15:0] shiftL1, shiftL2, shiftL3, shiftL4;
 
 ///////////////////////////////////////////
 // -- Connections/Combinational Logic -- //
 ///////////////////////////////////////////
 
 //------------------------------------------------------------------------------
+// First layer of shifting- 1 bit shifts.
+assign selL1 = i_opcode & {2{i_rhs[0]}};
+Mux4 M0[15:0] (
+	.A(i_lhs),                    // No Shift
+	.B({i_lhs[14:0], 1'b0}),      // Shift Logic Left
+	.C({i_lhs[15], i_lhs[15:1]}), // Shift Arith Right
+	.D({1'b0, i_lhs[15:1]}),      // Shift Logic Right
+	.S(selL1),
+	.Y(shiftL1)
+);
+
+//------------------------------------------------------------------------------
+// Second layer of shifting- 2 bit shifts.
+assign selL2 = i_opcode & {2{i_rhs[1]}};
+Mux4 M1[15:0] (
+	.A(shiftL1),                           // No Shift
+	.B({shiftL1[13:0], 2'b00}),            // Shift Logic Left
+	.C({{2{shiftL1[15]}}, shiftL1[15:2]}), // Shift Arith Right
+	.D({2'b00, shiftL1[15:2]}),            // Shift Logic Right
+	.S(selL2),
+	.Y(shiftL2)
+);
+
+//------------------------------------------------------------------------------
+// Third layer of shifting- 4 bit shifts.
+assign selL3 = i_opcode & {2{i_rhs[2]}};
+Mux4 M2[15:0] (
+	.A(shiftL2),                           // No Shift
+	.B({shiftL2[11:0], 4'b0000}),          // Shift Logic Left
+	.C({{4{shiftL2[15]}}, shiftL2[15:4]}), // Shift Arith Right
+	.D({4'b0000, shiftL2[15:4]}),          // Shift Logic Right
+	.S(selL3),
+	.Y(shiftL3)
+);
+
+//------------------------------------------------------------------------------
+// Fourth layer of shifting- 8 bit shifts.
+assign selL4 = i_opcode & {2{i_rhs[3]}};
+Mux4 M3[15:0] (
+	.A(shiftL3),                           // No Shift
+	.B({shiftL3[7:0], 8'b00000000}),       // Shift Logic Left
+	.C({{8{shiftL3[15]}}, shiftL3[15:8]}), // Shift Arith Right
+	.D({8'b00000000, shiftL3[15:8]}),      // Shift Logic Right
+	.S(selL4),
+	.Y(shiftL4)
+);
+
+//------------------------------------------------------------------------------
+// Return shifted value.
+assign o_result = shiftL4;
 
 endmodule
