beb2fcd588261fe4e01e8f6aca21019463741023
John Eslinger <johneslingertx@gmail.com>
2025-03-23 07:57:45 -0500

    Implemented tool chain changes to asmld- leaving off there to preserve workings of asmld

--- compiler/asmld/build.sh
+++ compiler/asmld/build.sh
@@ -1,14 +1,17 @@
-# (Internet one liner for getting script directory (no symlinks).
+#!/bin/bash
+
+# Plug-and-play parameters.
+PROJ_NAME="AsmldProject"
+EXEC_NAME="asmld"
+
+# (Internet one liner for getting script directory- no symlinks).
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-# Conduct build from known place- asmld/script's home directory.
+# Conduct build from known place- script's home directory.
 cd ${SCRIPT_DIR}
 
-# Clean builds ONLY.
-rm -rf build asmld.exe
-
 # Generate "version" for the program (not best, but will do for now).
-(echo '#define APP_VERSION "'`git log -n 1 --format=%x76%h`'"') > ./AsmldProject/version.h
+(echo '#define APP_VERSION "'`git log -1 --format=%cd --date=format:v%Y%m%d`'"') > ./${PROJ_NAME}/version.h
 
-# Serial of commands to get asmld.exe in its parent directory.
-cmake -S . -B build -DRUN=Build && cmake --build build && mv ./build/Debug/asmld.exe .
+# Serial of commands to get executable in home directory.
+cmake -S . -B build -G 'MSYS Makefiles' && cmake --build build && mv ./build/${EXEC_NAME}.exe .
