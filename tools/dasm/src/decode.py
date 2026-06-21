705f630d63562bfaf2fa95feb9547713be92565c
John Eslinger <johneslingertx@gmail.com>
2026-06-21 18:29:14 -0500

    ...forgot to finish refactor.

--- tools/dasm/src/decode.py
+++ tools/dasm/src/decode.py
@@ -3,26 +3,6 @@
 # by John Eslinger
 ################################################################################
 
-# Common ISA opcodes.
-__opcodes = {
-    0:  "NOP",
-    1:  "SWP",
-    2:  "BRC",
-    3:  "HLT",
-    4:  "STR",
-    5:  "LDR",
-    6:  "JPR",
-    7:  "JLR",
-    8:  "ADD",
-    9:  "SUB",
-    10: "LBI",
-    11: "XOR",
-    12: "SHL",
-    13: "SHR",
-    14: "ORR",
-    15: "AND"
-}
-
 ################################################################################
 # Interpret 16-bit word into instruction type (based on opcode).
 def __get_type(word: int):
